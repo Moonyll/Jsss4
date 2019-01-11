7a103c173ba1ba8fb8eefb85dcf2f3828b85b4d7
Moonyll <s.pouwel@laposte.net>
2019-01-11 15:59:37 +0100

    Jsss4

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+﻿function alert()
+{
+    if (document.getElementById('password').value == document.getElementById('confirmPassword').value)
+    {
+    document.getElementById('password').style.border = '3px solid green';
+    document.getElementById('confirmPassword').style.border = '3px solid green';
+    }
+    else
+    {
+     document.getElementById('password').style.border = '3px solid red';
+     document.getElementById('confirmPassword').style.border = '3px solid red';
+    }
+}
