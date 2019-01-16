a308afc0ab017266e7a366d9ca1ccaa8e459456b
Moonyll <s.pouwel@laposte.net>
2019-01-16 13:26:02 +0100

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-﻿function alert()
-{
-    if (document.getElementById('password').value == document.getElementById('confirmPassword').value)
+﻿function alert() {
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirmPassword');
+    if (password.value == confirmPassword.value)
     //condition si le mot de passe entré et la confirmation sont identiques.
     {
-    //création de la bordure verte.
-    document.getElementById('password').style.border = '3px solid green';
-    document.getElementById('confirmPassword').style.border = '3px solid green';
+        //création de la bordure verte.
+        password.style.border = '3px solid green';
+        confirmPassword.style.border = '3px solid green';
     }
     else
     //condition si le mot de passe entré et la confirmation sont differents.
     {
-    //création de la bordure rouge.
-     document.getElementById('password').style.border = '3px solid red';
-     document.getElementById('confirmPassword').style.border = '3px solid red';
+        //création de la bordure rouge.
+        password.style.border = '3px solid red';
+        confirmPassword.style.border = '3px solid red';
     }
 }
